b598e4ef04cd055028ce4ddd6f7f06e5aed84772
Per B. Sederberg <psederberg@gmail.com>
2012-11-12 07:12:25 -0500

    NF/BF: Added conditional looping and fixed bug in parent state timing.

--- smile/state.py
+++ smile/state.py
@@ -139,7 +139,10 @@ class State(object):
     def leave(self):
         # update the parent state time to actual elapsed time if necessary
         if self.duration < 0:
-            self.advance_parent_state_time(now()-self.start_time)
+            if isinstance(self, ParentState):
+                self.advance_parent_state_time(self.state_time-self.start_time)
+            else:
+                self.advance_parent_state_time(now()-self.start_time)
 
         # remove the callback from the schedule
         clock.unschedule(self.callback)
@@ -163,7 +166,6 @@ class ParentState(State):
         super(ParentState, self).__init__(interval=-1, parent=parent, 
                                           duration=duration, reset_clock=reset_clock)
         self.children = []
-        self.check = True
 
     def get_state_time(self):
         return self.state_time
@@ -182,6 +184,7 @@ class ParentState(State):
         # set all children to not done
         for c in self.children:
             c.done = False
+        self.check = True
 
     def __enter__(self):
         # push self as current parent
@@ -301,12 +304,14 @@ class If(ParentState):
         if self.false_state:
             self.claim_child(self.false_state)
 
+    def _enter(self):
+        # reset outcome so we re-evaluate if called in loop
+        self.outcome = val(self.cond)
+        super(If, self)._enter()
+        
     def _callback(self, dt):
         if self.check:
             done = False
-            # evaluate the conditional
-            if self.outcome is None:
-                self.outcome = val(self.cond)
 
             # process the outcome
             if self.outcome:
@@ -328,7 +333,8 @@ class If(ParentState):
 class LoopItem(object):
     def __init__(self, item):
         self.item = item
-
+        self.value = Ref(self,'item')
+        
     def __getitem__(self, index):
         # modify so that we're accessing item
         return Ref(gfunc = lambda : self.item[index])
@@ -339,23 +345,37 @@ class Loop(Serial):
         Show(Image(trial.current['image']), 2.0)
         Wait(.5)
     """
-    def __init__(self, dictlist, parent=None, reset_clock=False):
+    def __init__(self, iterable=None, conditional=True, 
+                 parent=None, reset_clock=False):
         super(Loop, self).__init__(parent=parent, duration=-1, 
                                    reset_clock=reset_clock)
 
         # otherwise, assume it's a list of dicts
-        self.dictlist = dictlist
+        self.iterable = iterable
+        self.cond = conditional
+        self.outcome = True
         
         # set to first in loop
-        if len(self.dictlist) == 0:
-            raise ValueError('The dictlist can not be zero length.')
-        self._i = 0
-        self.current = LoopItem(self.dictlist[self._i])
+        self.i = 0
+        if not self.iterable is None:
+            if len(self.iterable) == 0:
+                raise ValueError('The iterable can not be zero length.')
+            self.current = LoopItem(self.iterable[self.i])
         
+    def _enter(self):
+        # reset outcome so we re-evaluate if called in loop
+        self.outcome = val(self.cond)
+        super(Loop, self)._enter()
+
     def _callback(self, dt):
         if self.check:
-            # process the children
-            
+            # check the outcome each time
+            if not self.outcome:
+                # we should stop now
+                self.interval = 0
+                return
+                
+            # process the children            
             # start those that are not active and not done
             reset_next = False
             num_done = 0
@@ -383,15 +403,18 @@ class Loop(Serial):
 
             if num_done == len(self.children):
                 # we're done with this sequence
-                self._i += 1
-                if self._i >= len(self.dictlist):
-                    # we're really done
-                    self.interval = 0
-                else:
-                    # set to next
-                    self.current.item = self.dictlist[self._i]
-                    
-                    # reset all the child states
+                self.i += 1
+                if not self.iterable is None:
+                    # see if we're done with the loop
+                    if self.i >= len(self.iterable):
+                        # we're really done
+                        self.interval = 0
+                    else:
+                        # set to next
+                        self.current.item = self.iterable[self.i]
+
+                if self.interval != 0:
+                    # reset all the child states and update outcome
                     self._enter()
                     
         pass
@@ -438,34 +461,20 @@ if __name__ == '__main__':
 
     from experiment import Experiment
 
-    def print_dt(state, txt):
+    def print_dt(state, *txt):
         print txt, now()-state.state_time, state.dt
 
     exp = Experiment()
 
-    # # with explicit parents
-    # If(True, 
-    #    Func(print_dt, args=["True"], interval=0.0),
-    #    Func(print_dt, args=["False"], interval=0.0),
-    #    parent=exp)
-    # Wait(1.0, parent=exp)
-    # If(False, 
-    #    Func(print_dt, args=["True"], interval=0.0),
-    #    Func(print_dt, args=["False"], interval=0.0),
-    #    parent=exp)
-    # Wait(2.0, parent=exp)
-    # Func(print_dt, args=["two"], interval=0.0, parent=exp)
-    # Wait(3.0, parent=exp)
-    # with Parallel(parent=exp) as trial:
-    #     with Serial(parent=trial) as x:
-    #         Wait(1.0, parent=x)
-    #         Func(print_dt, args=['three'], interval=0.0, parent=x)
-    #     Func(print_dt, args=['four'], interval=0.0, parent=trial)
-    #     print trial.children
-    # Wait(2.0, stay_active=True, parent=exp)
-
-
     # with implied parents
+    block = range(3)
+    with Loop(block) as trial:
+        Func(print_dt, args=[trial.current.value])
+        Wait(1.0)
+        If(trial.current.value==block[-1],
+           Wait(2.))
+        
+    
     If(True, 
        Func(print_dt, args=["True"]),
        Func(print_dt, args=["False"]))
@@ -488,10 +497,38 @@ if __name__ == '__main__':
     with Loop(block) as trial:
         Func(print_dt, args=[trial.current['text']])
         Wait(1.0)
-
+        
     Wait(2.0, stay_active=True)
 
     exp.run()
+
+
+
+
+
+
+
+    # # with explicit parents
+    # If(True, 
+    #    Func(print_dt, args=["True"], interval=0.0),
+    #    Func(print_dt, args=["False"], interval=0.0),
+    #    parent=exp)
+    # Wait(1.0, parent=exp)
+    # If(False, 
+    #    Func(print_dt, args=["True"], interval=0.0),
+    #    Func(print_dt, args=["False"], interval=0.0),
+    #    parent=exp)
+    # Wait(2.0, parent=exp)
+    # Func(print_dt, args=["two"], interval=0.0, parent=exp)
+    # Wait(3.0, parent=exp)
+    # with Parallel(parent=exp) as trial:
+    #     with Serial(parent=trial) as x:
+    #         Wait(1.0, parent=x)
+    #         Func(print_dt, args=['three'], interval=0.0, parent=x)
+    #     Func(print_dt, args=['four'], interval=0.0, parent=trial)
+    #     print trial.children
+    # Wait(2.0, stay_active=True, parent=exp)
+
 
     
     pass
