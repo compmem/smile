a5fc7ff513fc6d46a5a258a26898593ef939eb72
Per B. Sederberg <psederberg@gmail.com>
2013-07-28 20:06:57 -0400

    RF: Added header

--- docs/examples/flip_test.py
+++ docs/examples/flip_test.py
@@ -1,4 +1,11 @@
-
+#emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See the COPYING file distributed along with the smile package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
 # load all the states
 from smile import *
