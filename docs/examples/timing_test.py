776a79619e380943f0a06aef04419c6b676d6628
Per B. Sederberg <psederberg@gmail.com>
2014-06-09 23:24:49 -0400

    NF: Slight mod to timing test to show image and add fast flips.

--- docs/examples/timing_test.py
+++ docs/examples/timing_test.py
@@ -20,6 +20,10 @@ trials = [{'dur':d,'isi':i}
           for d,i in zip([.005,.010,.020,.040,.080,.160,.320,.640],
                          [.005,.010,.020,.040,.080,.160,.320,.640])]
 
+# add in a bunch of fast switches
+trials = [{'dur':.005,'isi':.005}]*20 + trials
+
+# double length and repeat
 trials = trials*2
 trials_copy = trials[:]
 trials_copy.reverse()
@@ -27,17 +31,19 @@ trials.extend(trials_copy)
 
 # set initially to black
 BackColor(color=(0,0,0,1.0))
+#Show(Text('Jubba'), duration=1.0)
+Show(Image('face-smile.png'), duration=1.0)
 Wait(2.0, reset_clock=True)
 with Loop(trials) as trial:
-    Wait(.200)
+    # wait the isi
+    Wait(trial.current['isi'], reset_clock=True)
+    #Wait(.200)
     # turn it on
-    onstim = BackColor(color=(1,1,1,1.0), reset_clock=True)
+    onstim = BackColor(color=(1,1,1,1.0))
     # wait the dur
     Wait(trial.current['dur'])
     # turn if off
     offstim = BackColor(color=(0,0,0,1.0))
-    # wait the isi
-    Wait(trial.current['isi'])
 
     # log the on and off times
     Log(on=onstim['last_flip'],
