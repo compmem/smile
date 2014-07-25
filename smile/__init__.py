607080b6669d0ac0a4afae9e253dbf312adc2f1a
Per B. Sederberg <psederberg@gmail.com>
2014-07-25 08:02:49 -0400

    NF: Expose MousePress in init

--- smile/__init__.py
+++ smile/__init__.py
@@ -10,6 +10,7 @@
 from experiment import Experiment, Set, Get, Log
 from state import Parallel, Serial, If, Loop, Wait, Func, ResetClock
 from keyboard import KeyPress
+from mouse import MousePress
 from video import Show, Update, Unshow, Text, Image, Movie, BackColor
 from ref import Ref,val
 from freekey import FreeKey
