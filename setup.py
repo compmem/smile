61942f87005e9e04cae53178f1f2aeb049f6b5f4
Per B. Sederberg <psederberg@gmail.com>
2014-09-22 11:12:32 -0400

    RF: Preparing for install.

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+#emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See the COPYING file distributed along with the smile package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
-
-from distutils.core import setup, Extension
-from distutils.sysconfig import get_config_var
 import os
 import sys
+from distutils.core import setup, Extension
+from distutils.sysconfig import get_config_var, get_python_lib
+from distutils.dir_util import copy_tree
 
 setup(name='smile', 
       version='0.1.0',
@@ -13,4 +21,5 @@ setup(name='smile',
       maintainer=['Per B. Sederberg'],
       maintainer_email=['psederberg@gmail.com'],
       url=['http://github.com/compmem/smile'])
+
 
