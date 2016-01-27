7462a6551426e660a2d7b57e90f1759674154cfc
beegica <beegjacques@yahoo.com>
2016-01-27 10:46:44 -0500

    BF : mathdistrct would only make one set of questions in a loop.

--- smile/mathdistract.py
+++ smile/mathdistract.py
@@ -1,5 +1,5 @@
 from video import Label
-from state import Subroutine, If, Else, UntilDone, Loop, Wait
+from state import Subroutine, If, Else, UntilDone, Loop, Wait, Debug
 from audio import Beep
 from keyboard import KeyPress
 from ref import Ref
@@ -8,12 +8,12 @@ import random
 
 
 @Subroutine
-def MathDistract(self, 
-                 num_vars=2, 
+def MathDistract(self,
+                 num_vars=2,
                  min_num=1,
-                 max_num=9, 
+                 max_num=9,
                  max_probs=50,
-                 duration=30, 
+                 duration=30,
                  keys={'True':'F','False':'J'},
                  plus_and_minus=False,
                  text_size = None,
@@ -28,114 +28,109 @@ def MathDistract(self,
                  visual_feeback=True):
     """
     Math distractor for specified period of time.  Logs to a subroutine_0.slog
-    
+
     INPUT ARGS:
-      problemTimeLimit - set this param for non-self-paced distractor;
+      duration - set this param for non-self-paced distractor;
                          buzzer sounds when time's up; you get at least
                          minDuration/problemTimeLimit problems.
-      numVars - Number of variables in the problem.
-      maxNum - Max possible number for each variable.
-      minNum - Min possible number for each varialbe.
-      maxProbs - Max number of problems.
-      plusAndMinus - True will have both plus and minus.
-      minDuration - Minimum duration of distractor.
-      textSize - Vertical height of the text.
-      correctBeepDur - Duration of correct beep.
-      correctBeepFreq - Frequency of correct beep.
-      correctBeepRF - Rise/Fall of correct beep.
-      correctSndFile - Optional Audio clip to use for correct notification.
-      incorrectBeepDur - Duration of incorrect beep.
-      incorrectBeepFreq - Frequency of incorrect beep.
-      incorrectBeepRF - Rise/Fall of incorrect beep
-      incorrectSndFile - Optional AudioClip used for incorrect notification.
-      tfKeys - dictionary of keys for true/false problems. e.g., tfKeys = ('True':'T','False':'F')
-      ansMod - For True/False problems, the possible values to add to correct answer.
-      ansProb - The probability of each modifer on ansMod (must add to 1).
-      visualFeedback - Whether to provide visual feedback to indicate correctness.
-    
+      num_vars - Number of variables in the problem.
+      max_num - Max possible number for each variable.
+      min_num - Min possible number for each varialbe.
+      max_probs - Max number of problems.
+      plus_and_minus - True will have both plus and minus.
+      min_duration - Minimum duration of distractor.
+      text_size - Vertical height of the text.
+      correct_beep_dur - Duration of correct beep.
+      correct_beep_freq - Frequency of correct beep.
+      correct_beep_rf - Rise/Fall of correct beep.
+      incorrect_beep_dur - Duration of incorrect beep.
+      incorrect_beep_freq - Frequency of incorrect beep.
+      incorrect_beep_rf - Rise/Fall of incorrect beep
+      keys - dictionary of keys for true/false problems. e.g., tfKeys = ('True':'T','False':'F')
+      ans_mod - For True/False problems, the possible values to add to correct answer.
+      ans_prob - The probability of each modifer on ansMod (must add to 1).
+      visual_feedback - Whether to provide visual feedback to indicate correctness.
+
     OUTPUT ARGS
-      MathDistract.responces - A list of dictionaries containing the following:
+      MathDistract.responses - A list of dictionaries containing the following:
                                       - responses['correct'] == Boolean
                                       - responses['resp_time'] == Float (seconds)
                                       - responses['stim'] == String (The trial's stimulus)
     """
 
-
-
-
-
-    #Variables
     self.responses = []
-    #List Gen
-    trials=[]
-    for i in range(max_probs):
-        factors=[]
-        #generate the Factors of the Equation
-        for b in range(num_vars):
-            factors.append(random.randint(min_num, max_num+1))
-        opperators=[]
-        random.shuffle(factors)
-        total = factors[0]
-        #Generate num_factors - 1 number of operators
-        if plus_and_minus:
-            for x in range(num_vars-1):
-                if(random.randrange(2)==0):
-                    total = total + factors[x+1] 
-                    opperators.append('+')
-                else:
-                    total = total - factors[x+1] 
-                    opperators.append('-')
-        else:
-            rand_element = random.randrange(2)
-            for x in range(num_vars-1):
-                if rand_element:
-                    total = total + factors[x+1] 
-                    opperators.append('+')
-                else:
-                    total = total - factors[x+1] 
-                    opperators.append('-')
-        #Default to true, but if we randomly generate a 0,
-        #then condition = False and the total is squed by 
-        #
-        #condition = 'True'
-        #if(random.randrange(2) == 0):
-        #	condition= 'False'
-        #	if(random.randrange(2) == 0):
-        #		total = total + random.randrange(5,10)
-        #	else:
-        #		total = total - random.randrange(5,10)
-        last_prob = 0
-        rand_per = random.random()
-        for i in range(len(ans_prob)):
-            if rand_per > last_prob and rand_per < last_prob+ans_prob[i]:
-                new_total = total + ans_mod[i]
-            last_prob += ans_prob[i]        
-        condition=True
-        if new_total != total:
-            condition = False
-        
-        
-        # Build the Equation String
-        temp = str(factors[0])
-        for y in range(num_vars-1):
-            temp = temp + opperators[y] + str(factors[y+1])
-        temp = temp + '=' + str(new_total)
-        print temp
-        # Append the list of trials with the currently 
-        # generated stimuli
-        trials.append({
-                'text':temp,
-                'condition':condition,
-                'correct_key':keys[str(condition)]
-        })
+    def gen_questions(num_vars, max_num, min_num, max_probs, plus_and_minus, ans_mod, ans_prob):
+        #List Gen
+        trials=[]
+        for i in range(max_probs):
+            factors=[]
+            #generate the Factors of the Equation
+            for b in range(num_vars):
+                factors.append(random.randint(min_num, max_num+1))
+            opperators=[]
+            random.shuffle(factors)
+            total = factors[0]
+            #Generate num_factors - 1 number of operators
+            if plus_and_minus:
+                for x in range(num_vars-1):
+                    if(random.randrange(2)==0):
+                        total = total + factors[x+1]
+                        opperators.append('+')
+                    else:
+                        total = total - factors[x+1]
+                        opperators.append('-')
+            else:
+                rand_element = random.randrange(2)
+                for x in range(num_vars-1):
+                    if rand_element:
+                        total = total + factors[x+1]
+                        opperators.append('+')
+                    else:
+                        total = total - factors[x+1]
+                        opperators.append('-')
+            #Default to true, but if we randomly generate a 0,
+            #then condition = False and the total is squed by
+            #
+            #condition = 'True'
+            #if(random.randrange(2) == 0):
+            #	condition= 'False'
+            #	if(random.randrange(2) == 0):
+            #		total = total + random.randrange(5,10)
+            #	else:
+            #		total = total - random.randrange(5,10)
+            last_prob = 0
+            rand_per = random.random()
+            for i in range(len(ans_prob)):
+                if rand_per > last_prob and rand_per < last_prob+ans_prob[i]:
+                    new_total = total + ans_mod[i]
+                last_prob += ans_prob[i]
+            condition=True
+            if new_total != total:
+                condition = False
 
+
+            # Build the Equation String
+            temp = str(factors[0])
+            for y in range(num_vars-1):
+                temp = temp + opperators[y] + str(factors[y+1])
+            temp = temp + '=' + str(new_total)
+            # Append the list of trials with the currently
+            # generated stimuli
+            trials.append({
+                    'text':temp,
+                    'condition':condition,
+                    'correct_key':keys[str(condition)]
+            })
+        return trials
+    trials = Ref(gen_questions,num_vars, max_num, min_num, max_probs, plus_and_minus, ans_mod, ans_prob)
+    Debug(name='bob', text=trials)
     with Loop(trials) as trial:
         Label(text='+', duration=1.0, font_size=50, color='white')
         curtext = Label(text=trial.current['text'], font_size=50, color='white')
         with UntilDone():
             kp = KeyPress(keys=(keys['True'],keys['False']), correct_resp=trial.current['correct_key'])
             # If visual_feedback is True, display a the words Correct! or Incorrect! in red or green
-            # for the remainder of the trial duration. 
+            # for the remainder of the trial duration.
             with If(visual_feeback):
                 with If(kp.correct):
                     Label(text='Correct!', center_x=self.exp.screen.center_x, center_y=self.exp.screen.center_y/2.0,
@@ -143,7 +138,7 @@ def MathDistract(self,
                 with Else():
                     Label(text='Incorrect!', center_x=self.exp.screen.center_x, center_y=self.exp.screen.center_y/2.0,
                             font_size=60,duration=0.5,color='red')
-            # If visual_feedback is False, queue a beep to be played, at the specified frequencies, 
+            # If visual_feedback is False, queue a beep to be played, at the specified frequencies,
             # durations, and volumes depending on if they got it correct or incorrect.
             with Else():
                 with If(kp.correct):
@@ -168,10 +163,13 @@ def MathDistract(self,
 
 
 if __name__ == '__main__':
-    from smile import *
+    from experiment import Experiment
+    from state import Log
     exp = Experiment()
-    md = MathDistract(visual_feeback=False)
-    Log(name='Bababooie',
+    with Loop(3) as lp:
+        Label(text="Trial "+Ref(str,lp.i), duration=1)
+        md = MathDistract(visual_feeback=True, duration=10, max_probs=12)
+        Log(name='Bababooie',
         resp_time=md.responses)
     exp.run()
 
