6bb7ac06dc57454c42691f2f40501559b99823b7
Per B. Sederberg <psederberg@gmail.com>
2016-02-01 15:59:18 -0500

    NF/BF: Clean setup and add data files.

--- setup.py
+++ setup.py
@@ -7,19 +7,16 @@
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
-import os
-import sys
-from distutils.core import setup, Extension
-from distutils.sysconfig import get_config_var, get_python_lib
-from distutils.dir_util import copy_tree
+from distutils.core import setup
 
-setup(name='smile', 
+setup(name='smile',
       version='0.1.0',
-      package_dir={"smile":"smile"},
       packages=['smile'],
+      package_dir={"smile": "smile"},
+      package_data={"smile": ["face-smile.png",
+                              "test_sound.wav",
+                              "test_video.mp4"]},
       author=['Per B. Sederberg'],
       maintainer=['Per B. Sederberg'],
       maintainer_email=['psederberg@gmail.com'],
       url=['http://github.com/compmem/smile'])
-
-
