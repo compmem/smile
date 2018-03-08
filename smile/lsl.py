0dbee166b6f22d0663dc8ff622d1df4ccc27225b
beegica <bgj5hk@hipp.psyc.virginia.edu>
2018-03-08 14:34:28 -0500

    CH: Updated the names of the different states, cleaned up for pep8,

--- smile/lsl.py
+++ smile/lsl.py
@@ -14,10 +14,10 @@ from pylsl import StreamInfo, StreamOutlet
 
 
 def init_lsl_outlet(server_name, server_type, nchans,
-                      suggested_freq, dtype, unique_id="SMILE_LSL_OUT",):
+                    suggested_freq, dtype, unique_id="SMILE_LSL_OUT",):
     """Sends a Marker to a specified LSL.
 
-    A *LSL_Pulse* state will use a pre-initialized LSL outlet to send out a
+    A *LSLPush* state will use a pre-initialized LSL outlet to send out a
     marker to the Lab Streaming Layer.
 
     Parameters
@@ -51,22 +51,35 @@ def init_lsl_outlet(server_name, server_type, nchans,
         the same source id on the network again). Therefore, it is highly
         recommended to always try to provide whatever information can uniquely
         identify the data source itself.
+
+    Returns a StreamOutlet class that is to be used in conjunction with the
+    *LSLPush* state.
+
     """
+
+
     #info = StreamInfo('MyMarkerStream3','Markers',1,0,'int32','bababooi513')
     info = StreamInfo(server_name, server_type, nchans, suggested_freq,
                      dtype, unique_id)
     return StreamOutlet(info)
 
-class LSL_Pulse(CallbackState):
+
+
+class LSLPush(CallbackState):
     """Sends a Marker to a specified LSL.
 
-    A *LSL_Pulse* state will use a pre-initialized LSL outlet to send out a
+    A *LSLPush* state will use a pre-initialized LSL outlet to send out a
     marker to the Lab Streaming Layer.
 
     Parameters
     ----------
+    server : StreamOutlet
+        The server that you would like to push data out to. This can easily be
+        setup by running the *init_lsl_outlet* function defined in **lsl.py**.
     val : object
-        The value of the marker you would like to send to the LSL.
+        The value of the marker you would like to send to the LSL. Can either
+        be an object or a list of objects, depending on the nchans defined for
+        the server.
 
     Logged Attributes
     -----------------
@@ -75,7 +88,7 @@ class LSL_Pulse(CallbackState):
     recorded in the state-specific log. Refer to State class
     docstring for additional logged parameters.
 
-    pulse_time : Float
+    push_time : Float
         The time in which the pulse has finished sending to the LSL. This value
         is in seconds and based on experiment time. Experiment time is the
         number of seconds since the start of the experiment.
@@ -88,24 +101,29 @@ class LSL_Pulse(CallbackState):
     your experiment in order to *wake up* the LSL listener.
 
     """
+
+
     def __init__(self, server, val, **kwargs):
 
-        super(LSL_Pulse, self).__init__(parent=kwargs.pop("parent", None),
-                                   repeat_interval=kwargs.pop("repeat_interval", None),
-                                   duration=kwargs.pop("duration", 0.0),
-                                   save_log=kwargs.pop("save_log", True),
-                                   name=kwargs.pop("name", None),
-                                   blocking=kwargs.pop("blocking", True))
+        super(LSLPush, self).__init__(parent=kwargs.pop("parent", None),
+                                      repeat_interval=kwargs.pop("repeat_interval", None),
+                                      duration=kwargs.pop("duration", 0.0),
+                                      save_log=kwargs.pop("save_log", True),
+                                      name=kwargs.pop("name", None),
+                                      blocking=kwargs.pop("blocking", True))
         self._init_server = server
-        self._init_pulse_val = val
-        self._pulse_time = None
+        self._init_push_val = val
+        self._push_time = None
+
 
     def _callback(self):
-        if type(self._pulse_val) != list:
-            self._server.push_sample([self._pulse_val], clock.now())
+        if type(self._push_val) != list:
+            self._server.push_sample([self._push_val], clock.now())
         else:
-            self._server.push_sample(self._pulse_val, clock.now())
-        self._pulse_time = clock.now()
+            self._server.push_sample(self._push_val, clock.now())
+        self._push_time = clock.now()
+
+
 
 if __name__ == "__main__":
 
@@ -113,17 +131,33 @@ if __name__ == "__main__":
 
     exp = Experiment()
 
-    MARKER_OUT = init_lsl_outlet('MarkerStream','Markers',1,500,'int32','SMILE_LSL_OUT')
-    LSL_Pulse(MARKER_OUT, 55) # Needed if your system has a delay for the first pulse.
-    Wait(2.) # Wait for the real one!
+    # Initialize the outlet
+    OUTLET = init_lsl_outlet(server_name='MarkerStream',
+                             server_type='Markers',
+                             nchans=1,
+                             suggested_freq=500,
+                             dtype='int32',
+                             unique_id='SMILE_LSL_OUT')
+
+    # Signal the beginning of the experiment.
+    LSLPush(server=OUTLET,
+            val=55)
+
+    # Wait for the experiment to start!
+    Wait(2.)
+
     with Parallel():
-        Label(text="We will not pulse 10 times.", blocking=False)
+
+        Label(text="We will now push 10 markers.", blocking=False)
         with Loop(10, blocking=False):
-            pulse_out = LSL_Pulse(MARKER_OUT, 111)
+
+            # Create the push state
+            push_out = LSLPush(server=OUTLET,
+                               val=111)
 
             # Log like this if you want.
-            #Log(name="PULSES",
-            #    pulse_time=pulse_out.pulse_time)
+            #Log(name="MAKERS",
+            #    push_time=push_out.push_time)
 
             Wait(1.)
 
