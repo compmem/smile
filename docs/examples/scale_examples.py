0accdd5b4a02b35685281b9001871b202233d168
beegica <bgj5hk@hipp.psyc.virginia.edu>
2018-07-26 16:32:31 -0400

    DOC: Added comments to the example and scale.py

--- docs/examples/scale_examples.py
+++ docs/examples/scale_examples.py
@@ -20,15 +20,28 @@ from smile.scale import scale as s
 # your stimulus will look the same on all monitors that are bigger than the
 # scale box.
 
-
+# NOTE If you want to test out what your code would look like on a different
+# monitor (that is smaller than the one you already have up), you can run the
+# following line in the command line:
+#                python your_code.py -f -r **width**x**height**
+# Where width and height are replaced with the resolution of the monitor you
+# want to test. 
 
 RADIUS = 100
 
+
+# Set a scale_box to [600, 500] which is [width, height] and scale_up/scale_down
+# to make sure that no matter what the monitor size is, everything will be scale
+# to fit in side a scale_box.
 exp = Experiment(scale_box=[600,500], scale_up=True, scale_down=True)
 
+# As an example, if you run this file on one computer, and then another one
+# with a different monitor size, the first number in this debug should be
+# different on both machines, but the second number will be 10.
 Debug(s=s(10), b=10)
 
-
+# Font size is a perfect example of something you would want to scale with
+# monitor size and screen density.
 Label(text="These are some instructions", font_size=s(20))
 with UntilDone():
     if platform == "android":
@@ -37,8 +50,15 @@ with UntilDone():
     else:
         KeyPress()
 Wait(1.)
+
+# MovingDots allows us to to play with many different sizing and positioning
+# parameters. The scale of the dots, the radius of the apature, the speed of the
+# dots, all of these things are perfect for using scale.
 with Parallel():
     md1 = MovingDots(scale=s(50), radius=s(RADIUS), speed=s(100))
+    # Please note the md1.top + s(50). If you want to do relative positioning,
+    # then you must use scale if you want the relative distance between things
+    # to be equal.
     Label(text="50: "+Ref(str,s(50)), y=md1.top + s(50), font_size=s(40))
 with UntilDone():
     if platform == "android":
@@ -47,6 +67,9 @@ with UntilDone():
     else:
         KeyPress()
 Wait(1.)
+
+# The following are 3 more examples of what it would look like if we scaled
+# different parameters in a moving dot stimulus.
 with Parallel():
     md2 = MovingDots(scale=s(10), radius=s(RADIUS), speed=s(100))
     Label(text="10: "+Ref(str,s(10)), y=md2.top + s(50), font_size=s(40))
--- smile/scale.py
+++ smile/scale.py
@@ -1,4 +1,4 @@
-1# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # ex: set sts=4 ts=4 sw=4 et:
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
@@ -13,6 +13,29 @@ from kivy.metrics import dp
 class Scale(object):
     """
     Utility to help scale depending on various screen sizes.
+
+    Using a combination of Kivy's built in dp (density pixel) fuction and a
+    scaling factor, we are able to scale stimulus in SMILE to any device.
+    If you want to scale up or down to any monitor, you must set the scale_up
+    and/or scale_down flags when defining your Experiment:
+
+    ::
+
+        exp = Experiment(scale_up=True, scale_down=True, scale_box=(600, 500))
+
+    By doing so and wrapping all of your positional and sizing values your
+    experiment will be scaled such that the scale_box fits completely on the
+    screen you are running on.
+
+    If you only care about keeping the stimulus the same size on every device,
+    you should keep scale_up/down as False, and scale_box as None.
+
+    Wrap all raw positional and size values with scale like so:
+
+    ::
+
+        scale(100)
+
     """
     def __init__(self):
         self._scale_factor = 1.0
@@ -27,8 +50,13 @@ class Scale(object):
         self.scale_down = scale_down
 
     def _calc_scale_factor(self, width, height):
+        # If they offered a scale box
         if self._scale_box is not None:
+            # If scale_down is True then check to see if you need to scale down
             if self.scale_down == True:
+                # If both the density pixel height and width are bigger than the
+                # height and width of the screen, than check which one we
+                # should scale down.
                 if (dp(self._scale_box[1]) > height) & (dp(self._scale_box[0]) > width):
                     if (height / dp(self._scale_box[1])) < (width / dp(self._scale_box[0])):
                         self._scale_factor = height / dp(self._scale_box[1])
@@ -38,7 +66,7 @@ class Scale(object):
                     self._scale_factor = height / dp(self._scale_box[1])
                 elif dp(self._scale_box[0]) > width:
                     self._scale_factor = width / dp(self._scale_box[0])
-
+            # If scale_up is True
             if self.scale_up == True:
                 if (dp(self._scale_box[1]) < height) & (dp(self._scale_box[0]) < width):
                     if (height / dp(self._scale_box[1])) > (width / dp(self._scale_box[0])):
@@ -51,6 +79,8 @@ class Scale(object):
                     self._scale_factor = width / dp(self._scale_box[1])
 
     def __call__(self, val):
+        # Wrap the value in a ref. scale_factor is 1.0 by default, but we are
+        # always trying to scale a dp value, not a raw pixel value.
         return Ref(dp, val) * self._scale_factor_ref
 
 
