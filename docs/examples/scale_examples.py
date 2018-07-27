42c55d25666e8531080335b9fe7e5686cd2b7db0
beegica <bgj5hk@hipp.psyc.virginia.edu>
2018-07-27 09:44:03 -0400

    DOC: Fixed spelling errors in scale_example.

--- docs/examples/scale_examples.py
+++ docs/examples/scale_examples.py
@@ -5,19 +5,20 @@ from smile.ref import NotAvailable
 from smile.scale import scale as s
 
 
-# This is an example on how to use the scale function in smile. If you want
-# all of your sizing to be the same on every machine, you can set scale_box
-# to None. Using scale with no bounding box will change your numbers depending
-# on the pixel density of your screen using kivy's built in dp function. You
-# will want to wrap every number that has anything to do with sizing,
-# positioning, or moving in scale(x).
+# This is an example on how to use the scale function in smile. You can use the
+# scale function to make sure that your stimuli are the same size regardless of
+# screen density. You can also use scale to scale up or down your stimuli to
+# fill or fit into whatever size monitor you would like, as long as you set a
+# scale_box. scale_box is set in pixel space. If you keep all of your simuli
+# within the scale_box and you set scale_up/down to True, all of your simuli
+# will appear on any screen you run the experiment on.
 
 # Sometimes you might want to scale down only if the screen is too small to fit
-# your stimulus. In this case you set a *scale_box* and the scale_up/down flags.
+# your stimuli. In this case you set a *scale_box* and the scale_up/down flags.
 # If scale_down is True, **scale** will scale your numbers such that the dp on
 # your scale box is able to fit within the screen. If the screen is bigger than
 # the scale box, then your sizing numbers will only be scaled by **dp**, meaning
-# your stimulus will look the same on all monitors that are bigger than the
+# your stimuli will look the same on all monitors that are bigger than the
 # scale box.
 
 # NOTE If you want to test out what your code would look like on a different
@@ -25,7 +26,7 @@ from smile.scale import scale as s
 # following line in the command line:
 #                python your_code.py -f -r **width**x**height**
 # Where width and height are replaced with the resolution of the monitor you
-# want to test. 
+# want to test.
 
 RADIUS = 100
 
@@ -55,11 +56,11 @@ Wait(1.)
 # parameters. The scale of the dots, the radius of the apature, the speed of the
 # dots, all of these things are perfect for using scale.
 with Parallel():
-    md1 = MovingDots(scale=s(50), radius=s(RADIUS), speed=s(100))
+    md1 = MovingDots(scale=s(20), radius=s(RADIUS), speed=s(100))
     # Please note the md1.top + s(50). If you want to do relative positioning,
     # then you must use scale if you want the relative distance between things
     # to be equal.
-    Label(text="50: "+Ref(str,s(50)), y=md1.top + s(50), font_size=s(40))
+    Label(text="20: "+Ref(str,s(20)), y=md1.top + s(50), font_size=s(40))
 with UntilDone():
     if platform == "android":
         with ButtonPress():
