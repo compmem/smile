5f3122422fa62b1862aa7902e223809b052335f8
beegica <bgj5hk@virginia.edu>
2021-09-07 09:06:31 -0400

    RF: Allowed new kivy versions

--- smile/kivy_overrides.py
+++ smile/kivy_overrides.py
@@ -85,12 +85,12 @@ import kivy
 EXACT_KIVY_VERSIONS = (
     "1.8.0",
     "1.9.0",
-    "1.9.1-dev0",
     "1.9.1",
     "1.10.0",
-    "1.10.1.dev0",
     "1.10.1",
-    "1.11.1")
+    "1.11.0",
+    "1.11.1",
+    "2.0.0")
 if kivy.__version__ not in EXACT_KIVY_VERSIONS:
     raise ImportError("kivy version must be one of %r, got %r" %
                       (EXACT_KIVY_VERSIONS, kivy.__version__))
