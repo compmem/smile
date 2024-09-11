08b27e351c3c7c460286819e78c2ff656e80f600
dyld-w <iamdylandw@gmail.com>
2024-09-11 14:57:51 -0400

    DOC: Fixes documentation, variable names, & formatting

--- docs/examples/stern/config.py
+++ docs/examples/stern/config.py
@@ -11,11 +11,11 @@ ITEMS = string.ascii_lowercase
 
 # Get the directory where this script is located
 script_dir = Path(__file__).parent.absolute()
-# instructions writen in another document
+# instructions written in another document
 instruct_text = open(script_dir / 'stern_instructions.rst', 'r').read()
 
-RSTFONTSIZE = 30
-RSTWIDTH = 900
+RST_FONT_SIZE = 30
+RST_WIDTH = 900
 STUDY_DURATION = 1.2
 STUDY_ISI = .4
 RETENTION_INTERVAL = 1.0
@@ -26,4 +26,4 @@ RESP_DELAY = .2
 ORIENT_DURATION = 1.0
 ORIENT_ISI = .5
 ITI = 1.0
-FONTSIZE = 30
+FONT_SIZE = 30
--- docs/examples/stern/stern.py
+++ docs/examples/stern/stern.py
@@ -1,50 +1,49 @@
-
-# global imports
-import random
-import string
 # load all the states
 from smile.common import *
 
-#execute both the configuration file and the
-#stimulus generation file
+# execute both the configuration file and the
+# stimulus generation file
 from config import *
 from gen_stim import *
 
-#Define the experiment
+# Define the experiment
 exp = Experiment()
-#Present the instructions to the participant
-init_text = RstDocument(text=instruct_text, width=RSTWIDTH, font_size=RSTFONTSIZE, top=exp.screen.top, height=exp.screen.height)
+# Present the instructions to the participant
+init_text = RstDocument(text=instruct_text, width=RST_WIDTH,
+                        font_size=RST_FONT_SIZE, top=exp.screen.top, height=exp.screen.height)
 with UntilDone():
-    #Once the KeyPress is detected, the UntilDone
-    #cancels the RstDocument
+    # Once the KeyPress is detected, the UntilDone
+    # cancels the RstDocument
     keypress = KeyPress()
 # loop over study block
 with Loop(trials) as trial:
-    #Setup the list of study times.
+    # Setup the list of study times.
     exp.study_times = []
     # orient stim
-    orient = Label(text='+',duration=ORIENT_DURATION, font_size=FONTSIZE)
+    orient = Label(text='+', duration=ORIENT_DURATION, font_size=FONT_SIZE)
     Wait(ORIENT_ISI)
     # loop over study items
     with Loop(trial.current['study_items']) as item:
         # present the letter
-        ss = Label(text=item.current, duration=STUDY_DURATION, font_size=FONTSIZE)
+        ss = Label(text=item.current, duration=STUDY_DURATION,
+                   font_size=FONT_SIZE)
         # wait some jittered amount
         Wait(STUDY_ISI)
         # append the time
-        exp.study_times+=[ss.appear_time['time']]
+        exp.study_times += [ss.appear_time['time']]
     # Retention interval
     Wait(RETENTION_INTERVAL - STUDY_ISI)
     # present the letter
-    test_stim = Label(text=trial.current['test_item'], bold=True, font_size=FONTSIZE)
+    test_stim = Label(
+        text=trial.current['test_item'], bold=True, font_size=FONT_SIZE)
     with UntilDone():
         # wait some before accepting input
         Wait(RESP_DELAY)
         # Wait until the test_stim label has the appear_time attribute
         Wait(until=test_stim.appear_time)
-        #After the KeyPress is detected, the UntilDone
-        #cancels the Label test_stim and allows the
-        #experiment to continue.
+        # After the KeyPress is detected, the UntilDone
+        # cancels the Label test_stim and allows the
+        # experiment to continue.
         ks = KeyPress(keys=RESP_KEYS,
                       base_time=test_stim.appear_time['time'])
     # Log the trial
@@ -55,10 +54,10 @@ with Loop(trials) as trial:
         orient_time=orient.appear_time['time'],
         study_times=exp.study_times,
         test_time=test_stim.appear_time['time'],
-        correct=(((trial.current['condition']=='target')&
-                 (ks.pressed==RESP_KEYS[0])) |
-                 ((trial.current['condition']=='lure')&
-                 (ks.pressed==RESP_KEYS[1]))))
+        correct=(((trial.current['condition'] == 'target') &
+                 (ks.pressed == RESP_KEYS[0])) |
+                 ((trial.current['condition'] == 'lure') &
+                 (ks.pressed == RESP_KEYS[1]))))
     Wait(ITI)
 # run that exp!
 exp.run()
